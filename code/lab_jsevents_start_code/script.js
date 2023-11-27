a875efa3d739d9df3cdfddcf38452e1e29152976
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-11-27 15:11:23 +0000

    list shows user input from #new-todo

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -3,14 +3,16 @@ const list = document.querySelector('#list');
 const input = document.querySelector('#new-todo')
 const form = document.querySelector('#todo-form')
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
     const newListItem = document.createElement("li");
-    newListItem.innerText = "test";
+    
+    newListItem.innerText = input.value;
+
     list.appendChild(newListItem);
     console.log("button clicked")
-    
+    console.log(input);
 })
 
